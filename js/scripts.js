44042f2f7579b81977bad67a01365f837c8623eb
Sar-ah <Sarahhattab@ymail.com>
2015-02-15 15:23:05 +0100

    maj index/menu/popups

--- js/scripts.js
+++ js/scripts.js
@@ -12,124 +12,102 @@ var size = 960;
 var pack = d3.layout.pack()
     .sort(null)
     .size([size, size])
-    .value(function(d) { return d.radius * d.radius; })
+    .value(function(d) { console.log(d); return 5; })
     .padding(5);
 
   var csv = [], // this array will contain the csv data
-    svg = d3.select('#playground')
-    .append('svg')
-	.attr("width", size)
-    .attr("height", size)
-	.attr("transform", "translate(2,2)");
- 
+      svg = d3.select('#playground')
+        .append('svg')
+          .attr("width", size)
+          .attr("height", size)
+          .attr("transform", "translate(2,2)");
+     
   /*
     Get csv data as res (resource)
   */
-  function draw() {
-    console.log('Hello, drawing', csv.length, 'rows');
-    // transform data objects and group them year by year
-	//console.log(csv)
-	
-	 var auteurs_groups = {},
-      auteurs = [];
+  function draw(something) {
+    console.log('draw function, drawing', something.length, 'elements', something);
+    
+    //console.log(pack.nodes({children:something}))
 
-  // cycle through the csv array and find groups belonging to the same author
-  csv.filter(function(d){
-    if(!auteurs_groups[d.auteur])
-        auteurs_groups[d.auteur] = [] ;
-    auteurs_groups[d.auteur].push(d)
-  })
+<<<<<<< HEAD
+<<<<<<< HEAD
 
-  // object to array transformation, to be used with d3
-  for(var i in auteurs_groups) {
-    auteurs.push({
-        id:i,
-        items:auteurs_groups[i]
-    })
-  }
-  console.log(auteurs)
+=======
+>>>>>>> FETCH_HEAD
+    var node = svg.datum({
+      name: 'root',
+      children: something
+    }).selectAll(".node")
+      .data(pack.nodes)
+    .enter().append("g")
+      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+<<<<<<< HEAD
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";})
+	  .on("click", function(d,i) {
+	  
+	var meta = "Auteur : "+ d.auteur +"\nLivre : "+d.titre+"\nAnnee de publication : " +d.annee_publication +"\nFilm : " +d.film+"\nRealisateur : " +d.realisateur+"\nAnnee (film) : " +d.sortie;
+	console.log(meta);
+	d3.select("#ticket").text(meta);
+	//alert("Auteur : "+ d.auteur +"\nLivre : "+d.titre+"\nAnnee de publication : " +d.annee_publication +"\nFilm : " +d.film+"\nRealisateur : " +d.realisateur+"\nAnnee (film) : " +d.sortie); 
+	}); // automatic depuis pack layout
 		
-var livre = {};
-	csv.filter(function(d){
-		if(!livre[d.titre])
-			livre[d.titre] = [] ;
-		livre[d.titre].push(d)
-		
-	})
-		    console.log(livre)
-			
-			
-var films = {};
-	csv.filter(function(d){
-		if(!films[d.film])
-			films[d.film] = [] ;
-		films[d.film].push(d)
-		//console.log(d,d.film)
-	})  
-		    console.log(films)		
-			
-var selection = svg.selectAll("g.popcorn").data(auteurs)
-.enter()
-.append("g").attr({class:"popcorn"});
+    node.append("title")
+        .text(function(d) {
+          if(d.film) {
+            return d.film
+          }
+          return (d.name || d.film) +  (d.children ? '(' + d.children.length + ')': "")});
+=======
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";}); // automatic depuis pack layout
 
+    node.append("title")
+        .text(function(d) {
+          if(d.realisateur) {
+            return d.realisateur
+          }
+          return (d.name || d.realisateur) +  (d.children ? '(' + d.children.length + ')': "")});
+>>>>>>> FETCH_HEAD
 
-selection.append("circle").attr({
-  r: function (d,i){
-    return d.items.length
-  }, 
-  cy :20,
-  cx : function(d, i){console.log("",d,i);return i*40 ;}
-})      
+    node.append("circle")
+        .attr("r", function(d) { return d.r; }); // automatic depuis pack layout
 
-selection.selectAll('circle.item').data(function(d) {
-  return d.items
-})
-  .enter()
-  .append("circle")
-    .attr({class:"item"});
-	
+    node.filter(function(d) { return !d.children; }).append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+<<<<<<< HEAD
+        .text(function(d) { console.log(d); return d.film.substring(0, d.r / 3); });
+=======
+    var node = svg.datum({
+      name: 'root',
+      children: something
+    }).selectAll(".node")
+      .data(pack.nodes)
+    .enter().append("g")
+      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";}); // automatic depuis pack layout
 
-/*function update(){
+    node.append("title")
+        .text(function(d) {
+          if(d.realisateur) {
+            return d.realisateur
+          }
+          return (d.name || d.realisateur) +  (d.children ? '(' + d.children.length + ')': "")});
 
+    node.append("circle")
+        .attr("r", function(d) { return d.r; }); // automatic depuis pack layout
 
-  // note the use of key function and that we RESELECT
-  var uselection = selection.selectAll("circle.item").data(function(d) {return ''+d;});
-  console.log('selection without data is', selection, 'while uselection is', uselection);
-  
-  uselection.exit()
-  	.transition()
-  	
-  	.duration(500)
-  	.attr({
-    	x: 50,
-    	//y : function(d, i){return i * 30 + 30}
-  	})
-  	.remove()
-  // only already existing stuffs
-  uselection
-  	.transition()
-  	
-  	.duration(500)//function(d, i){return 100 * i})
-  	.attr({
-    	y : function(d, i){return i * 30 + 30}
-  	})
-  
-  uselection.enter()
-  	.append("circle")
-  	.attr({
-    	x: 20,
-    	y : function(d, i){return i * 30 + 30}
-  	})
-  	.selectAll(function(d){return d})
- 	
-  }
-setInterval(update, 1500);
-*/
-  }
+    node.filter(function(d) { return !d.children; }).append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function(d) { console.log(d); return d.realisateur.substring(0,5) });
+>>>>>>> FETCH_HEAD
+=======
+        .text(function(d) { console.log(d); return d.realisateur.substring(0,5) });
+>>>>>>> FETCH_HEAD
+  };
   
   
-
-
   // load csv file using d3
   d3.csv('contents/data.csv', function(res) {
     if(!res) {
@@ -138,8 +116,77 @@ setInterval(update, 1500);
     }
     // save locally csv data
     csv = res;
-    // let's call our draw function 
-    draw(); 
+
+    var auteurs_groups = {},
+        auteurs = [],
+        livres_groups = {},
+        livres = [],
+        films_groups = {},
+        films = [];;
+
+    // cycle through the csv array and find groups belonging to the same author
+    csv.filter(function(d){
+      if(!auteurs_groups[d.auteur])
+          auteurs_groups[d.auteur] = [];
+      auteurs_groups[d.auteur].push(d)
+    })
+
+    csv.filter(function(d){
+      if(!livres_groups[d.titre])
+        livres_groups[d.titre] = [];
+      livres_groups[d.titre].push(d);
+    });
+
+    csv.filter(function(d){
+      if(!films_groups[d.film])
+        films_groups[d.film] = [] ;
+      films_groups[d.film].push(d)
+    }) 
+
+    // object to array transformation, to be used with d3
+    for(var i in auteurs_groups) {
+      auteurs.push({
+        name: i,
+        children: auteurs_groups[i]
+      })
+    };
+    
+    for(var i in livres_groups) {
+      livres.push({
+        name: i,
+        children: livres_groups[i]
+      })
+    };
+    
+    for(var i in films_groups) {
+      films.push({
+        name: i,
+        children: films_groups[i]
+      })
+    };
+    
+    // groups
+    console.log("combien d'auteurs :", auteurs.length);
+    console.log('... de livres :', livres.length);
+    console.log('... et de films :', films.length);
+<<<<<<< HEAD
+<<<<<<< HEAD
+	
+
+    // call out 'update' function
+	
+    draw(auteurs);
+	
+=======
+    
+    // call out 'update' function
+    draw(auteurs);
+>>>>>>> FETCH_HEAD
+=======
+    
+    // call out 'update' function
+    draw(auteurs);
+>>>>>>> FETCH_HEAD
   });
 
 });
